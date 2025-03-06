8bc205236add419de4403ef2103120177b6cd18b
Ana Paola Hernández Hernández <pauchas836@gmail.com>
2025-03-06 10:24:20 -0600

    agregar input a materias

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("mostrarInput").addEventListener("click", function() {
+    document.querySelectorAll(".mostrarInput").forEach(function(button) {
+        button.addEventListener("click", function() {
         let form = document.getElementById("formulario");
         form.style.display = (form.style.display === "none" || form.style.display === "") ? "block" : "none";
+        });
     });
 });
