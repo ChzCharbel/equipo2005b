545d872c6c4bc17c2a6802a34b2531332af1e5b9
liliird <A01711949@tec.mx>
2025-03-27 23:52:12 -0600

    Centrar profesores

--- controllers/maestros.controller.js
+++ controllers/maestros.controller.js
@@ -1,9 +1,17 @@
-
-
+const Profesor = require("../models/profesores.model");
 
 exports.get_all_maestros = (request, response, next) => {
-    response.render('profesores.ejs', {
-        titulo: 'maestros',
+  Profesor.fetchAll()
+    .then((profesores) => {
+      console.log(profesores.rows);
+      request.session.profesores = profesores.rows;
+      response.render("profesores.ejs", {
+        titulo: "maestros",
         privilegios: request.session.privilegios || [],
+        profesores: request.session.profesores || [],
+      });
+    })
+    .catch((error) => {
+      console.log(error);
     });
 };
