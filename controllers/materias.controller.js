981975083731e16a77b0159b5514f8d51ba7dd62
Ana Paola Hernández Hernández <pauchas836@gmail.com>
2025-03-27 20:39:45 -0600

    lista de profesores para los grupos

--- controllers/materias.controller.js
+++ controllers/materias.controller.js
@@ -1,14 +1,21 @@
 const Materia = require('../models/materias.model');
+const Profesor = require('../models/profesores.model');
 const { getAllCourses } = require('../util/admin.api.client');
 
 exports.get_materias = (request, response, next) => {
     Materia.getAllCourses().then(materias => {
         console.log(materias);
-        response.render('materias.ejs', {
-        titulo: 'materias',
-        materias: materias || [],
-        privilegios: request.session.privilegios || [],
-        });
+        Profesor.fetchAll().then((profesores) => {
+            console.log(profesores)
+            response.render('materias.ejs', {
+                titulo: 'materias',
+                materias: materias || [],
+                privilegios: request.session.privilegios || [],
+                profesores: profesores.rows || [],
+                });
+        }).catch((error) => {
+            console.log(error);
+        })
     });
     
 }
