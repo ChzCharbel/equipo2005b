cb88a8b6da59939a6e35d8925d829e043de181a9
ChzCharbel <charbel_chz@outlook.com>
2025-04-07 02:36:18 -0600

    comillas agregadas

--- models/alumnos.model.js
+++ models/alumnos.model.js
@@ -2,19 +2,19 @@ const db = require('../util/database');
 
 module.exports = class Alumno {
     static fetchAll() {
-		return db.query(`SELECT u."nombreUsuario", a.matricula, a.carrera, a.regular, a.semestre
-    FROM "Alumno" a, "Usuario" u WHERE a.matricula = u."idIVD";`);
+		return db.query(`SELECT u."nombreUsuario", a."matricula", a."carrera", a."regular", a."semestre"
+    FROM "Alumno" a, "Usuario" u WHERE a."matricula" = u."idIVD";`);
 	}
 	static fetchOne(id){
-		return db.query(`SELECT u."nombreUsuario", a.matricula, a.carrera, a.regular, a.semestre
-    FROM "Alumno" a, "Usuario" u WHERE a.matricula = u."idIVD" AND a."matricula" = $1::text`, [id]);
+		return db.query(`SELECT u."nombreUsuario", a."matricula", a."carrera", a."regular", a."semestre"
+    FROM "Alumno" a, "Usuario" u WHERE a."matricula" = u."idIVD" AND a."matricula" = $1::text`, [id]);
 	}
 
 	static find(nombreUsuario) {
         return db.query(
-            `SELECT u."nombreUsuario", a.matricula, a.carrera, a.regular, a.semestre
+            `SELECT u."nombreUsuario", a."matricula", a."carrera", a."regular", a."semestre"
 			FROM "Usuario" u, "Alumno" a
-			WHERE a.matricula = u."idIVD" AND u."nombreUsuario" LIKE $1::text`,
+			WHERE a."matricula" = u."idIVD" AND u."nombreUsuario" LIKE $1::text`,
             [nombreUsuario + '%']);
     }
 }
