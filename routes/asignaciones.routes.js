8c92bbbcbef3936bacfdf6532dd62dea24613c57
Ana Paola Hernández Hernández <pauchas836@gmail.com>
2025-04-10 02:59:35 -0600

    borarr documentos que no servian

--- routes/asignaciones.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { asignarProfesorSalon } = require('../controllers/asignaciones.controller');
-
-router.post('/asignar', asignarProfesorSalon);
-
-module.exports = router;
